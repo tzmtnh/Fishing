0a73f495c1af2cf73214a1a374568fa687fd4e45
Matan' Laptop <matan@onehamsa.com>
2018-04-14 12:09:45 -0700

    Adding Game States

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -8,15 +8,33 @@ public class CameraController : MonoBehaviour {
 
 	Vector3 _pos;
 
+	void updateFishing() {
+		float deltaY = Rob.inst.hook.transform.position.y - _pos.y;
+		if (deltaY < minHookY) {
+			_pos.y += deltaY - minHookY;
+			transform.position = _pos;
+		}
+	}
+
 	void Start () {
 		_pos = transform.position;
 	}
 	
 	void Update () {
-		float deltaY = Rob.inst.hook.transform.position.y - _pos.y;
-		if (deltaY < minHookY) {
-			_pos.y += deltaY - minHookY;
-			transform.position = _pos;
+		switch (GameManager.inst.state) {
+			case GameManager.GameState.StartMenu:
+				break;
+			case GameManager.GameState.Fishing:
+				updateFishing();
+				break;
+			case GameManager.GameState.Ninja:
+				break;
+			case GameManager.GameState.EndGame:
+				break;
+			case GameManager.GameState.Leaderboard:
+				break;
+			default:
+				break;
 		}
 	}
 }
--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -5,9 +5,11 @@ using UnityEngine.Assertions;
 
 public class GameManager : MonoBehaviour {
 
-	public enum GameState { StartMenu, InGame }
+	public enum GameState { StartMenu, Fishing, Ninja, EndGame, Leaderboard }
 
 	public static GameManager inst;
+
+	public GameState state;
 
 	private void Awake() {
 		Assert.IsNull(inst);
