39424519a3c78705f0114c144d9dd3a6e723566c
Matan' Laptop <matan@onehamsa.com>
2018-04-14 00:35:38 -0700

    basic Rob added

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour {
+
+	public float minHookY = -1;
+
+	Vector3 _pos;
+
+	void Start () {
+		_pos = transform.position;
+	}
+	
+	void Update () {
+		float deltaY = Rob.inst.hook.transform.position.y - _pos.y;
+		if (deltaY < minHookY) {
+			_pos.y += deltaY - minHookY;
+			transform.position = _pos;
+		}
+	}
+}
---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Assertions;
+
+public class GameManager : MonoBehaviour {
+
+	public enum GameState { StartMenu, InGame }
+
+	public static GameManager inst;
+
+	private void Awake() {
+		Assert.IsNull(inst);
+		inst = this;
+	}
+}
