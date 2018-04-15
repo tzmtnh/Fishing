f2ced6699faa9936ec537154a2977fbba93b83dc
Matan' Laptop <matan@onehamsa.com>
2018-04-15 01:35:07 -0700

    Showing final score in end game UI

---
+++ Assets/Scripts/EndGameUI.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class EndGameUI : MonoBehaviour {
+
+	public Text scoreText;
+
+	void OnEnable() {
+		if (scoreText != null) {
+			scoreText.text = GameManager.inst.score + "$";
+		}
+	}
+}
