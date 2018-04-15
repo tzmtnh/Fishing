26bfefe0ee1e526a9070a3609034345f5e199ed9
Matan' Laptop <matan@onehamsa.com>
2018-04-15 15:53:58 -0700

    balancing

--- Assets/Scripts/Garbage.cs
+++ Assets/Scripts/Garbage.cs
@@ -4,14 +4,18 @@ using UnityEngine;
 
 public class Garbage : Entity {
 
+	public bool spin = true;
 	float _angularDrag;
 
 	void Start() {
 		_sprite.flipX = Random.value > 0.5f;
-		_rigidbody.rotation = Random.value * 360f;
-		_rigidbody.angularVelocity = Random.Range(-1f, 1f) * 30;
-		_angularDrag = _rigidbody.angularDrag;
-		_rigidbody.angularDrag = 0;
+
+		if (spin) {
+			_rigidbody.rotation = Random.value * 360f;
+			_rigidbody.angularVelocity = Random.Range(-1f, 1f) * 30;
+			_angularDrag = _rigidbody.angularDrag;
+			_rigidbody.angularDrag = 0;
+		}
 	}
 
 	public override void attachTo(Rigidbody2D rb, Collider2D c) {
